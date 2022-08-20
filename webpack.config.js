1151428c96537214bf48f690796f0fee12fe8bf2
David Kasilia <83514256+David-Kasilia@users.noreply.github.com>
2022-08-20 21:41:29 +0300

    Project Setup Complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,80 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+
+    static: './dist',
+
+  },
+
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          }
+        ]
+        
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+
+    ],
+
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    })
+  ],
+  optimization: {
+
+    runtimeChunk: 'single',
+
   },
 };
